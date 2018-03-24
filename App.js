fb615a5d5a722b4a971fd056960d73b0766b4f8a
Filipe de Sousa Botelho <filipegame07@gmail.com>
2018-03-23 23:34:13 -0300

    Finalizando app

--- App.js
+++ App.js
@@ -1,33 +1,71 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   Image,
-  Platform,
   StyleSheet,
+  Text,
   TextInput,
+  TouchableHighlight,
   View
 } from 'react-native';
 
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View
-        style={{
-          flex: 1,
-        }}
-      >
-        <View style={{ flex: 2, alignItems: 'center', marginTop: 70, }}>
-          <Image source={require('./imgs/fbimg.png')} />
-        </View>
-        <View style={{ flex: 1, backgroundColor: 'rgb(254, 240, 252)'}}>
-          <TextInput
-            placeholder="Login ou Email"
-          />
-        </View>
+export default props => (
+  <View style={{ flex: 1 }}>
+    <View style={styles.viewImagem}>
+      <Image style={styles.imagem}source={require('./imgs/fbimg.png')} />
+    </View>
+    <View style={styles.viewFormulario}>
+      <View>
+        <TextInput
+          style={[styles.textInput, { marginBottom: 10 }]}
+          placeholder="Login ou Email"
+          underlineColorAndroid="#1ddfec"
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Senha"
+          underlineColorAndroid="#1ddfec"
+          secureTextEntry
+        />
       </View>
-    );
-  }
-}
+      <TouchableHighlight
+        style={styles.button}
+        onPress={() => false}
+        underlayColor="#005ad1"
+      >
+        <Text style={styles.textoBotao}>Entrar</Text>
+      </TouchableHighlight>
+    </View>
+  </View>
+);
 
 const styles = StyleSheet.create({
-
+  textInput: {
+    backgroundColor: '#1ddfec',
+    marginHorizontal: 30,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 30,
+    marginBottom: 5,
+    height: 40,
+    backgroundColor: '#006eff'
+  },
+  textoBotao: {
+    color: '#FFF',
+    fontWeight: '700'
+  },
+  imagem: {
+    height: 150,
+    width: 150,
+  },
+  viewImagem: {
+    flex: 5,
+    alignItems: 'center',
+    marginTop: 70,
+  },
+  viewFormulario: {
+    flex: 2,
+    justifyContent: 'space-between',
+  },
 });
